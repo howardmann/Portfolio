7cd6f8b4cab85ff7d590c369b17f15b23d59c69c
howardmann <howardmann27@gmail.com>
2016-10-19 09:44:19 +1100

    Mobile changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 var s;
 // Initialize skrollr library
 $(document).ready(function(){
-  s = skrollr.init();
+  s = skrollr.init([
+    smoothScrolling=true,
+    mobileCheck=function(){
+       return (/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera);
+    }
+  ]);
 });
