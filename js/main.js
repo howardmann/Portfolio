761287cedd9e9b524bf3e565ffa51fa39d6209a9
howardmann <howardmann27@gmail.com>
2017-01-25 11:38:04 +1100

    Change JS file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
+console.log("ready");
 var s;
 // Initialize skrollr library
 $(document).ready(function(){
   s = skrollr.init();
+  var block = $('.about-me');
+  var resizeBackground = function(){
+    block.height($(window).height() - 60);
+  };
+
+  $(window).resize(resizeBackground);
+  resizeBackground();
 });
